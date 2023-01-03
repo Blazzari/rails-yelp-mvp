99bd3945398072edefacab808e2aad15d045d9ae
Lazzari <baptiste.lazzari@outlook.fr>
2023-01-03 21:06:49 +0100

    exo 2_2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+seed_restaurants = [
+  { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', phone_number: '0134861275', category: 'chinese' },
+  { name: 'Cabouche', address: '8 epic, London E2 7JE', phone_number: '0134861355', category: 'italian' },
+  { name: 'Sushhi', address: '26 downtown, London E2 7JE', phone_number: '0134552898', category: 'japanese' },
+  { name: 'La bonne soupe', address: '89 le champ, London E2 7JE', phone_number: '0134552833', category: 'french' },
+  { name: 'Fritte du chef', address: '18 la fritte, London E2 7JE', phone_number: '0134552844', category: 'belgian' }
+]
+
+seed_restaurants.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
